1e14b46155a54f962b051b1af57d0ae847f6fe1c
Axel Beckert <abe@deuxchevaux.org>
2010-11-14 23:44:52 +0100

    Add opacity to unfocused windows

--- rc.lua
+++ rc.lua
@@ -355,6 +355,15 @@ client.add_signal("manage", function (c, startup)
     end
 end)
 
-client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.add_signal("focus", function(c)
+      c.border_color = beautiful.border_focus
+      c.opacity = 1
+end)
+client.add_signal("unfocus", function(c)
+      c.border_color = beautiful.border_normal
+      c.opacity = 0.8
+end)
+
+-- client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+-- client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}

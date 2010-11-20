e22a33c8da0aa540079d303562d7725db1dba9ce
Axel Beckert <abe@deuxchevaux.org>
2010-11-20 20:02:53 +0100

    Add script to renumber ratpoison windows according to my standard layout.
    0 => root shell
    1 => Conkeror web browser
    2 => ssh to mail client
    3 => ssh to IRC client
    4 => mirror sync logs
    10 => GNU Emacs

---
+++ bin/renumber-ratpoison-windows.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -wl
+
+use strict;
+
+my %data = (
+    q/root@nemo:/ => 0,
+    q/.* - Conkeror$/ => 1,
+    q/abe@nemo: autossh -t sym 'screen -RdU'/ => 2,
+    q/abe@nemo: autossh -t kiva6 'screen -RdU'/ => 3,
+    q(ftp\.ch\.debian\.org: rsync/ftpsync log$) => 4,
+    q/emacs\@nemo\.deuxchevaux\.org$/ => 10,
+);
+
+my $issues = 1;
+issue: while ($issues) {
+    my $ratpoison_windows = `ratpoison -c windows`;
+    foreach my $check (keys %data) {
+	if ($ratpoison_windows !~ /^($data{$check})\D$check/m and
+	    $ratpoison_windows =~ /^([0-9]+)\D($check)/m and
+	    $1 != $data{$check}) {
+	    print "$1 => $data{$check} ($2)";
+	    system(qw(ratpoison -c), "number $data{$check} $1");
+	    next issue;
+	}
+    }
+    $issues = 0;
+}

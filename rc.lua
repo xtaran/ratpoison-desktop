3a2cf6550e3bf32d93e80b579f154c693a89de92
Axel Beckert <abe@deuxchevaux.org>
2011-01-11 02:28:33 +0100

    Get modkey-0 working

--- rc.lua
+++ rc.lua
@@ -298,6 +298,36 @@ for i = 1, keynumber do
                       end
                   end))
 end
+
+
+globalkeys = awful.util.table.join(globalkeys,
+    awful.key({ modkey }, "#19",
+              function ()
+                    local screen = mouse.screen
+                    if tags[screen][10] then
+                        awful.tag.viewonly(tags[screen][10])
+                    end
+              end),
+    awful.key({ modkey, "Control" }, "#19",
+              function ()
+                  local screen = mouse.screen
+                  if tags[screen][10] then
+                      awful.tag.viewtoggle(tags[screen][10])
+                  end
+              end),
+    awful.key({ modkey, "Shift" }, "#19",
+              function ()
+                  if client.focus and tags[client.focus.screen][10] then
+                      awful.client.movetotag(tags[client.focus.screen][10])
+                  end
+              end),
+    awful.key({ modkey, "Control", "Shift" }, "#19",
+              function ()
+                  if client.focus and tags[client.focus.screen][10] then
+                      awful.client.toggletag(tags[client.focus.screen][10])
+                  end
+              end))
+
 for i = 11, 22 do
     globalkeys = awful.util.table.join(globalkeys,
         awful.key({ modkey }, "F" .. i - 10,

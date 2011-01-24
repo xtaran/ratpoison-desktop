5d49a47aa01c016537c8e40647fe1e325e6111d1
Axel Beckert <abe@deuxchevaux.org>
2011-01-24 16:21:31 +0100

    Some placement rules (Emacs, Liferea, Vigor)

--- rc.lua
+++ rc.lua
@@ -353,13 +353,17 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { class = "Vigor" },
+      properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     { rule = { class = "Firefox" },
       properties = { tag = tags[1][2] } },
     { rule = { class = "Conkeror" },
       properties = { tag = tags[1][2] } },
-    { rule = { class = "Emacs" },
-      properties = { tag = tags[1][4] } },
+    { rule = { class = "Liferea" },
+      properties = { tag = tags[1][3] } },
+--    { rule = { class = "Emacs" },
+--      properties = { tag = tags[1][4] } },
 }
 -- }}}
 

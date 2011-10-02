3ff7c66e13c031dcd0011b751d0b847d665f211b
Axel Beckert <abe@deuxchevaux.org>
2011-10-02 06:50:46 +0200

    Convert dash to underscore for slice names

--- bin/slice-config-file.sh
+++ bin/slice-config-file.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-HOSTNAME=`( test -z "$2" && uname -n || echo $2 ) | tr a-z A-Z`
-DIST=`( test -z "$3" && lsb_release -cs || echo $3 ) | tr a-z A-Z`
+HOSTNAME=`( test -z "$2" && uname -n || echo $2 ) | tr a-z- A-Z_`
+DIST=`( test -z "$3" && lsb_release -cs || echo $3 ) | tr a-z- A-Z_`
 
 slice -o "UNDEF+$HOSTNAME+$DIST-NOT$DIST-NOT$HOSTNAME:-" $1

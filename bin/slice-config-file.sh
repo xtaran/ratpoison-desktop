83e801063163a953c4ec624b7a2521a18f0249c7
Axel Beckert <abe@deuxchevaux.org>
2011-10-02 06:48:03 +0200

    slice: Use UNDEF instead of ALL

--- bin/slice-config-file.sh
+++ bin/slice-config-file.sh
@@ -3,4 +3,4 @@
 HOSTNAME=`( test -z "$2" && uname -n || echo $2 ) | tr a-z A-Z`
 DIST=`( test -z "$3" && lsb_release -cs || echo $3 ) | tr a-z A-Z`
 
-slice -o "ALL+$HOSTNAME+$DIST-NOT$DIST-NOT$HOSTNAME:-" $1
+slice -o "UNDEF+$HOSTNAME+$DIST-NOT$DIST-NOT$HOSTNAME:-" $1

817e972d78284e5d947a6540fec5f86a23836079
Axel Beckert <beckert@phys.ethz.ch>
2013-06-01 01:48:08 +0200

    Jump to 2x2 instead of 0x0 to avoid accidental screen switching

--- bin/focus-follows-mouse-on-tag-change.sh
+++ bin/focus-follows-mouse-on-tag-change.sh
@@ -4,6 +4,6 @@
 # the window and back in
 xdotool getmouselocation | awk -F'[: ]' '{print $2" "$4" "$6}' | while read x y s; do
     echo $x $y $s
-    xdotool mousemove --screen $s 0 0
+    xdotool mousemove --screen $s 2 2
     xdotool mousemove --screen $s $x $y
 done

71a6f9c9e4b4c9f0a323a1a9eb817bd55e536d9a
Axel Beckert <beckert@phys.ethz.ch>
2013-06-01 01:48:43 +0200

    Remove more debug code

--- bin/focus-follows-mouse-on-tag-change.sh
+++ bin/focus-follows-mouse-on-tag-change.sh
@@ -3,7 +3,6 @@
 # Regain focus on the window under the mouse by moving the mouse off
 # the window and back in
 xdotool getmouselocation | awk -F'[: ]' '{print $2" "$4" "$6}' | while read x y s; do
-    echo $x $y $s
     xdotool mousemove --screen $s 2 2
     xdotool mousemove --screen $s $x $y
 done

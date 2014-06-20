dd8c6382ea63622e9ffa3c35e3e8950cfad41c20
Axel Beckert <abe@deuxchevaux.org>
2014-06-20 15:04:34 +0200

    awesome rc: New windows are slave windows by default

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -441,7 +441,7 @@ client.add_signal("manage", function (c, startup)
     if not startup then
         -- Set the windows at the slave,
         -- i.e. put it at the end of others instead of setting it master.
-        -- awful.client.setslave(c)
+        awful.client.setslave(c)
 
         -- Put windows in a smart way, only if they does not set an initial position.
         if not c.size_hints.user_position and not c.size_hints.program_position then

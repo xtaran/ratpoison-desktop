e7ee157698aa1b841ff63292e9e032bfc1549a14
Axel Beckert <abe@deuxchevaux.org>
2015-03-02 11:52:36 +0100

    Use a dark GNU Emacs theme

--- emacs.el
+++ emacs.el
@@ -11,10 +11,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(column-number-mode t)
  '(debian-changelog-allowed-distributions
    (quote
     ("UNRELEASED" "unstable" "testing" "testing-security" "stable" "stable-security" "oldstable-security" "experimental")))
+ '(custom-enabled-themes (quote (wombat)))
  '(display-battery-mode t)
  '(display-time-24hr-format t)
  '(display-time-mode t)

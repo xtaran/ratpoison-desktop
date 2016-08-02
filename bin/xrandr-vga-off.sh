8d8e62696bf14c7c43540631b30ef67024ec700a
Axel Beckert <abe@deuxchevaux.org>
2016-08-02 07:55:59 +0200

    xrandr-vga-off: Sometimes the internal display is also eDP1-1 instead of eDP1

--- bin/xrandr-vga-off.sh
+++ bin/xrandr-vga-off.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
-INTERNAL="`xrandr | fgrep connected | egrep    '^(eDP1|LVDS-?1)' | awk '{print $1}'`"
-EXTERNAL="`xrandr | fgrep connected | egrep -v '^(eDP1|LVDS-?1)' | awk '{print $1}'`"
+INTERNAL="`xrandr | fgrep connected | egrep    '^(eDP-?1|LVDS-?1)' | awk '{print $1}'`"
+EXTERNAL="`xrandr | fgrep connected | egrep -v '^(eDP-?1|LVDS-?1)' | awk '{print $1}'`"
 
 for output in $INTERNAL; do
     xrandr --output $output --auto

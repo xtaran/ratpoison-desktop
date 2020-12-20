12e4398ee705cb56afb9b60df1f94f17a184e898
Axel Beckert <abe@deuxchevaux.org>
2020-12-20 23:59:21 +0100

    Fonts: Prefer Agave for Emacs, RobotoBold for i3
    Also pull in Jetbrains Mono for future use.

--- emacs.el
+++ emacs.el
@@ -129,4 +129,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Agave" :foundry "agrc" :slant normal :weight normal :height 120 :width normal)))))

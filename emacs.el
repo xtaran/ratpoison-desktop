1e48715062be05f20a8e12d381013e90353d576d
Axel Beckert <abe@deuxchevaux.org>
2024-11-24 23:20:02 +0100

    Drop old no more existing x-cut-buffer-or-selection-value causing issues

--- emacs.el
+++ emacs.el
@@ -56,7 +56,6 @@
 ; mouse-sel-mode resets it to nil and while I want most of
 ; mouse-sel-mode's feature, but IMHO resetting these is annoying.
 ;(setq interprogram-cut-function 'x-set-cut-buffer)
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
 
 ; Markdown Mode for .md files
 (autoload 'markdown-mode "markdown-mode.el"
